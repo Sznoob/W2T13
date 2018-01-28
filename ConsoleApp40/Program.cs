77c7fa4b0f212d79ea2d3608a192f48bca2a43ea
Joni Kananen <L7043@student.jamk.fi>
2018-01-28 13:07:15 +0200

    Week2

--- ConsoleApp40/Program.cs
+++ ConsoleApp40/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp40
     {
         static void Main(string[] args)
         {
+            int[] points = new int[5];
+            for (int i = 0; i < points.Length; i++)
+            {
+                Console.Write(" Give points {0}: ", i+1);
+                string line = Console.ReadLine();
+                int point;
+                bool result = Int32.TryParse(line, out point);
+                if (result)
+                {
+                    points[i] = point;
+                }
+                 else
+                    Console.WriteLine("You didn't give a number! ");
+            }
+            Array.Sort(points);
+
+            int sum = points[1] + points[2] + points[3];
+            Console.WriteLine("Sum is: " + sum) ;
+
+            Console.ReadLine();
         }
     }
 }
